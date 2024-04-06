376303cda863a39ffca4861b11948dc7f03b44fc
Yathish G P <yathishgp1@gmail.com>
2024-04-06 21:38:08 +0530

    first commite

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { ThemeProvider } from "@/providers/theme-provider"
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+
+const font = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: 'Fuzzie',
+  description: 'Automate Your Work With Fuzzie.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
